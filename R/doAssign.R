f1b232b0df1981f2f7596b797f5970d71358990c
RolandASc <roland.schmid@mirai-solutions.com>
2017-08-23 22:10:25 +0200

    RSc: simplification of assignments

--- R/doAssign.R
+++ R/doAssign.R
@@ -6,10 +6,23 @@
 #' @return \code{invisible(NULL)}
 #' @details First unlocks the binding and then performs the assignment
 #' in the specified environment
-doAssign = function(x, value, envir) {
+doAssign <- function(x, value, envir) {
   stopifnot(length(x) == 1)
   unlockBinding(x, envir)
   assign(x, value, envir = envir)
   # we don't care about re-locking bindings in god-mode
   invisible()
+}
+
+assignVal <- function(envs, string, value) {
+  for (i in seq(along = envs)) {
+    doAssign(string, value, envir = envs[[i]])
+  }
+}
+
+assignFunc <- function(envs, string, value) {
+  for (i in seq(along = envs)) {
+    environment(value) <- environment(get(string, envir = envs[[i]]))
+    doAssign(string, value, envir = envs[[i]])
+  }
 }
