5e46d1696a7b09cf13df9c6ff5a8d683ac0eaf84
RolandASc <roland.schmid@mirai-solutions.com>
2018-01-05 14:29:22 +0100

    comment out examples of failing tests

--- tests/testthat/test-stochModel.R
+++ tests/testthat/test-stochModel.R
@@ -5,8 +5,8 @@ suppressMessages({
 context("stochModel")
 
 # god-mode
-#godmode.on(pv = TRUE)
-godmode.on(pv = FALSE)
+#godmode.on(pv = FALSE)
+godmode.on(pv = TRUE)
 
 if (FALSE) {
   # Execute the command below to run the tests
@@ -59,9 +59,8 @@ test_that("Output is as expected", {
   expect_equal(res1, res2)
   expect_equal(res1, exp123, tolerance = 0.00000001, scale = 1, info = "rounded")
   # test expectation failing example:
-  expect_equal(res1, res3)
+  # expect_equal(res1, res3)
   # note below expectation only holds true as long as messaging isn't re-routed to cat!
-  # expect_message(godmode::stochModel(123), "idx is 31")
+  expect_message(godmode::stochModel(123), "idx is 31")
 
 })
-
